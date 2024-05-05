ad1c71f34e3491694ad142ffe712c4ad86cdae18
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-05-06 00:13:49 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
