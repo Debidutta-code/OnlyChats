04cb394ae7bffe6502ef7926b85f376914849f91
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-05-06 01:54:42 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from '../src/assets/of_logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={Logo} className="logo" alt="Logo" />
+      <h1 className="heading">
+        <span className="heading1">Only</span>
+        <span className="heading2">Chats</span>{" "}
+      </h1>
     </div>
   );
 }
-
+  
 export default App;
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,7 @@
+const Homepage = () => {
+    return(
+        <div className="homepage">
+            <h1>Homepage</h1>
+        </div>
+    )
+}
