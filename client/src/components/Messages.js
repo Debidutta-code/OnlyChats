ed4fa24e309d1e477d68f573df47e38308820023
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-05-21 22:07:55 +0530

    demo123

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -28,7 +28,7 @@ const MessageOptionsMenu = ({ handleLeaveMessage, handleDeleteChat }) => {
     );
 };
 
-const ENDPOINT = "https://onlychats.netlify.app";
+const ENDPOINT = "https://onlychats.vercel.app";
 var socket, selectedChatCompare;
 
 const MessagesComponent = ({ setIsProfileClicked, isAnyOnesChatOpen, contactClicked, userId, setContactFullDetails, setIsAnyOnesChatOpen, allMessages, setAllMessages, setChatRoomFullDetails }) => {
