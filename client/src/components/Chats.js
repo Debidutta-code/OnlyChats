def8c7a43c41f60f07bf7bc26cb0fb9a81114f7b
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-05-23 15:55:32 +0530

    minor change in message.js

--- client/src/components/Chats.js
+++ client/src/components/Chats.js
@@ -47,7 +47,7 @@ const ChatsList = ({ setIsProfileClicked, setIsAnyOnesChatOpen, isNewChatCreated
                 }
             }
             else {
-                setAllMessages([...allMessages, newMessageReceived]);
+                // setAllMessages([...allMessages, newMessageReceived]);
             }
         })
     });
