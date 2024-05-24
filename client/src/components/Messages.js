5d24ffc9fb4f14893e0eca5ef6f289803b4b9c4b
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-05-24 12:53:33 +0530

    demo change in message.js

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -75,6 +75,7 @@ const MessagesComponent = ({ setIsProfileClicked, isAnyOnesChatOpen, contactClic
             }
         } )
     });
+    
 
     useEffect(() => {
         messagesEndRef.current?.scrollIntoView();
